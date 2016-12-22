3682f17b06b022562eae1d04a9bd16f311ed4a31
Wedge <shadowpilot34@gmail.com>
2016-12-22 14:23:02 +0300

    Fix error

--- lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
@@ -2,7 +2,7 @@ class ActiveRecord::ConnectionAdapters::NullDBAdapter
 
   TableDefinition = ActiveRecord::ConnectionAdapters::TableDefinition.dup
   TableDefinition.instance_eval do
-    define_method :jsonb, -> {}
+    define_method :jsonb, ->(a) {}
   end
 
 end
